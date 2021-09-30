73fe8d003c2d53e9b900bb247339147507c72e51
Alex Chubaty <achubaty@for-cast.ca>
2021-09-30 10:41:01 -0600

    remove default 'path' in m3ToBiomPlots()

--- R/m3ToBiomPlots.R
+++ R/m3ToBiomPlots.R
@@ -17,12 +17,17 @@ utils::globalVariables(c(
 #' @importFrom data.table copy melt
 #' @importFrom ggforce facet_wrap_paginate
 #' @importFrom ggplot2 aes element_text facet_wrap geom_line ggplot ggsave labs theme
+#' @importFrom magrittr %>%
 #' @importFrom reproducible checkPath
 #'
 m3ToBiomPlots <- function(inc = "increments", id_col = "gcids", nrow = 5, ncol = 5,
-                          filenameBase = "rawCumBiomass_", path = figPath,
+                          filenameBase = "rawCumBiomass_", path = NULL,
                           title = "Cumulative merch fol other by gc id",
                           scales = "free_y") {
+  if (is.null(path)) {
+    stop("a valid 'path' must be specified.")
+  }
+
   gInc <- copy(inc)
   colsOut <- c("id", "ecozone")
   gInc[ ,(colsOut) := list(NULL,NULL)]
