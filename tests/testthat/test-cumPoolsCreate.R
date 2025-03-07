5a85c7b1949f7abe1fcae0cbaa5a1de79cc48c00
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-03-07 11:41:15 -0800

    add existing test

--- tests/testthat/test-cumPoolsCreate.R
+++ tests/testthat/test-cumPoolsCreate.R
@@ -74,3 +74,53 @@ test_that("testing getParameters function", {
   expect_message(getParameters(table6AGB, table7AGB, 101, 4, "notaprovince"))
   expect_error(getParameters(table6AGB, table7AGB, 1, 4, "BC"))
 })
+
+test_that("testing convertAGB2pools function", {
+  dt <- data.table(
+    expand.grid(canfi_species = c(204), # PINU_CON
+                age = c(3,15, 35),
+                ecozone = 4,
+                juris_id = "AB"
+    )
+  )
+  dt$B <- round(runif(nrow(dt), 1, 100))
+  out <- convertAGB2pools(dt, table6 = table6AGB, table7 = table7AGB)
+
+  #sum of the pools equal total AGB
+  expect_equal(rowSums(out), dt$B)
+  expect_true(out[dt$age < 15, "totMerch"] ==  0)
+  expect_true(all(colnames(out) == c("totMerch", "fol", "other")))
+  expect_true(all(!is.na(out)))
+  expect_equal(dim(out), c(3,3))
+
+})
+
+test_that("testing cumPoolsCreateAGB function", {
+  dt <- data.table(
+    expand.grid(canfi_species = c(204, 1201), # PINU_CON, POPU_TRE
+                age = c(3,15, 35),
+                ecozone = 4,
+                juris_id = "AB",
+                poolsPixelGroup = c(1,2)
+    )
+  )
+  dt$B <- round(runif(nrow(dt), 1, 100))
+  dt$speciesCode[dt$canfi_species == 204] <- "PINU_CON"
+  dt$speciesCode[dt$canfi_species == 1201] <- "POPU_TRE"
+  setorder(dt, speciesCode, age, poolsPixelGroup)
+
+  out2 <- cumPoolsCreateAGB(dt, table6 = table6AGB, table7 = table7AGB)
+
+  expect_equal(rowSums(out2[,c("totMerch", "fol", "other")]), dt$B/2)
+  expect_true(all(out2[dt$age < 15, "totMerch"] ==  0))
+  expect_equal(nrow(out2), nrow(dt))
+  expect_true(all(colnames(out2) == c("species", "age", "poolsPixelGroup", "totMerch", "fol", "other")))
+
+  dt$yieldPixelGroup <- dt$poolsPixelGroup
+  dt$poolsPixelGroup <- NULL
+  dt$cohort_id <- c(1:nrow(dt))
+  out3 <- cumPoolsCreateAGB(dt, table6 = table6AGB, table7 = table7AGB, pixGroupCol = "yieldPixelGroup")
+  expect_equal(out3$gcids, dt$cohort_id)
+  expect_equivalent(out2, out3[,-c("gcids")])
+
+})
